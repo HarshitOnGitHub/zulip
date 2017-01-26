56a38e349bf2df73678b08a2b8fc8a4269f340f7
Harshit Bansal <harshitbansal2015@gmail.com>
2017-01-26 10:00:22 +0000

    admin: Make restricted to domains checkbox readonly incase of no domains.
    If there are no domains then there is no meaning of setting restricted to
    domains checkbox and hence it should be disabled.
    
    Fixes: #3436.

--- static/js/admin.js
+++ static/js/admin.js
@@ -194,8 +194,11 @@ exports.populate_realm_aliases = function (aliases) {
         return (alias.allow_subdomains ? "*." + alias.domain : alias.domain);
     });
     var domains = domains_list.join(', ');
+
+    $("#id_realm_restricted_to_domain").prop("checked", page_params.realm_restricted_to_domain);
     if (domains.length === 0) {
         domains = i18n.t("None");
+        $("#id_realm_restricted_to_domain").prop("disabled", true);
     }
     $("#realm_restricted_to_domains_label").text(i18n.t("New users restricted to the following domains: __domains__", {domains: domains}));
 
@@ -934,6 +937,7 @@ function _setup_page() {
             success: function () {
                 $("#new_alias").val("");
                 $("#allow_subdomains").prop("checked", false);
+                $("#id_realm_restricted_to_domain").prop("disabled", false);
                 aliases_info.removeClass("text-error");
                 aliases_info.addClass("text-success");
                 aliases_info.text("Added successfully!");
